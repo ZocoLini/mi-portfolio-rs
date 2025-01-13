c3d294a0023e08452877f1f7816c0a524b50ebf9
Borja Castellano <borjacastellano1@gmail.com>
2025-01-13 20:54:32 +0100

    Improved the deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,13 @@
 
 cd crates/frontend || exit
 trunk build --release
+
+ssh root@lebastudios.org "
+        rm -rf /var/www/lebastudios.org/portfolio
+" || error_exit "SSH commands failed"
+
 scp -r dist/* root@lebastudios.org:/var/www/lebastudios.org/portfolio
 
+ssh root@lebastudios.org "
+        chown -R www-data:www-data /var/www/lebastudios.org/portfolio
+" || error_exit "SSH commands failed"
