d4be45501b77e8bc693332b943e8f00dbbadff80
Borja Castellano <borjacastellano1@gmail.com>
2025-06-29 21:10:04 +0200

    deploy script updated to use the dns

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 cd crates/frontend || exit
 trunk build --release
 
-ssh root@91.116.216.159 "
+ssh root@nginx.master.lebastudios.lan "
         rm -rf /var/www/lebastudios.org/portfolio
 " || error_exit "SSH commands failed"
 
-scp -r dist/* root@91.116.216.159:/var/www/lebastudios.org/portfolio
+scp -r dist/* root@nginx.master.lebastudios.lan:/var/www/lebastudios.org/portfolio
 
-ssh root@91.116.216.159 "
+ssh root@nginx.master.lebastudios.lan "
         chown -R www-data:www-data /var/www/lebastudios.org/portfolio
 " || error_exit "SSH commands failed"
