ed4adf2590abec2de53b65e33649061cc6d43da3
Borja Castellano <borjacastellano1@gmail.com>
2025-10-07 16:54:46 -0700

    compositor and fee projects added

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-# 
+
+SERVER="200.234.230.139"
+
 trunk build --release
 
-ssh root@nginx.master.lebastudios.lan "
+ssh root@$SERVER "
         rm -rf /var/www/lebastudios.org/portfolio
 " || error_exit "SSH commands failed"
 
-scp -r dist/* root@nginx.master.lebastudios.lan:/var/www/lebastudios.org/portfolio
+scp -r dist/* root@$SERVER:/var/www/lebastudios.org/portfolio
 
-ssh root@nginx.master.lebastudios.lan "
+ssh root@$SERVER "
         chown -R www-data:www-data /var/www/lebastudios.org/portfolio
 " || error_exit "SSH commands failed"
