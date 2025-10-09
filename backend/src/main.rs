71b675b4a01f8137a7702863850396ac20a04d77
Borja Castellano <borjacastellano1@gmail.com>
2025-10-08 18:33:55 -0700

    qr endpoint to track if the portfolio is reaching ppl

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use actix_web::{App, HttpResponse, HttpServer, get, http::header, middleware, web};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct PortfolioRedirectQuery {
+    name: Option<String>,
+}
+
+#[get("/api/v1/portfolio/qr")]
+async fn portfolio_qr(query: web::Query<PortfolioRedirectQuery>) -> HttpResponse {
+    let query = &*query;
+    let req_id = match query.name.as_ref() {
+        Some(name) => name,
+        None => "unknown",
+    };
+
+    if !req_id.contains("abc123.") {
+        println!("z: {}", req_id);
+    }
+
+    HttpResponse::TemporaryRedirect()
+        .insert_header((header::LOCATION, "https://lebastudios.org/portfolio"))
+        .finish()
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    println!("Listening on 127.0.0.1:8080");
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(middleware::Compress::default())
+            .service(portfolio_qr)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .workers(2)
+    .run()
+    .await
 }
--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,8 @@ set -e
 
 MODE=$1
 
+mkdir -p dist
+
 (
     cd frontend || exit 1
     if [ "$MODE" = "release" ]; then
@@ -11,5 +13,20 @@ MODE=$1
         trunk build -d ../dist
     fi
     
-    trunk serve -d ../dist
+    trunk serve -d ../dist &
+    echo "TRUNK process ID: $!"
 )
+
+(
+    cd backend || exit 1
+    if [ "$MODE" = "release" ]; then
+        cargo build --release -p backend
+    else
+        cargo build -p backend
+    fi
+)
+
+mkdir -p dist/backend
+cp target/debug/backend dist/backend/backend
+dist/backend/backend &
+echo "BACKEND process ID: $!"
