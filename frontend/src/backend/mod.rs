39946f825d5510ef2a16710d42aa86f344083d52
Borja Castellano <borjacastellano1@gmail.com>
2025-10-25 01:40:17 +0000

    method to obtain the host

--- frontend/src/backend/mod.rs
+++ frontend/src/backend/mod.rs
@@ -1,6 +1,6 @@
 use gloo_net::http::Request;
 use serde::Serialize;
-use web_sys::RequestCredentials;
+use web_sys::{RequestCredentials, window};
 use yew::platform::spawn_local;
 
 #[derive(Serialize)]
@@ -13,14 +13,16 @@ pub fn register_content_view(content_id: &str) {
         content_id: content_id.into(),
     };
 
-    #[cfg(debug_assertions)]
-    let endpoint = "http://127.0.0.1:8080/api/v1/portfolio/track/content-view";
+    let host = if let Some(host) = get_host() {
+        host
+    } else {
+        return;
+    };
 
-    #[cfg(not(debug_assertions))]
-    let endpoint = "https://bcastellano.com/api/v1/portfolio/track/content-view";
+    let endpoint = format!("http://{}/api/v1/portfolio/track/content-view", host);
 
     spawn_local(async move {
-        let _ = Request::post(endpoint)
+        let _ = Request::post(&endpoint)
             .header("Content-Type", "application/json")
             .credentials(RequestCredentials::Include)
             .body(serde_json::to_string(&payload).unwrap())
@@ -28,4 +30,24 @@ pub fn register_content_view(content_id: &str) {
             .send()
             .await;
     });
+}
+
+pub fn get_host() -> Option<String> {
+    if let Some(win) = window() {
+        if let Ok(mut host) = win.location().host() {
+            #[cfg(debug_assertions)]
+            {
+                host = host.replace(":8000", ":8080");
+            }
+            Some(host)
+        } else {
+            #[cfg(debug_assertions)]
+            web_sys::console::log_1(&"no host".into());
+            None
+        }
+    } else {
+        #[cfg(debug_assertions)]
+        web_sys::console::log_1(&"no active window".into());
+        None
+    }
 }
